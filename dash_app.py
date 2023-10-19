3c8bde8713ce6f217819a6b6f0549f6a47ffaff9
asia-ko <zykova.doc@gmail.com>
2023-10-19 15:39:55 +0200

    app

--- dash_app.py
+++ dash_app.py
@@ -1,5 +1,6 @@
 # Importing the libraries
 
+import pandas as pd 
 import dash
 from dash import Dash, dcc, html, callback
 from dash.dependencies import Input, Output, State
@@ -8,104 +9,66 @@ from dash import dash_table
 import dash_bootstrap_components as dbc
 
 # reading the data and preparing it for the tables and graphs
-df = px.data.gapminder()
-df_germany = df[df['country']=='Germany']
-df_germany = df_germany[['year', 'lifeExp', 'pop', 'gdpPercap']]
-df_countries =df[df['country'].isin(['Germany', 'Belgium', 'Denmark'])]
-
-# creating the table
-table = dash_table.DataTable(df_germany.to_dict('records'),
-                                  [{"name": i, "id": i} for i in df_germany.columns],
-                               style_data={'color': 'white','backgroundColor': "#222222"},
-                              style_header={
-                                  'backgroundColor': 'rgb(210, 210, 210)',
-                                  'color': 'black','fontWeight': 'bold'}, 
-                                     style_table={ 
-                                         'minHeight': '400px', 'height': '400px', 'maxHeight': '400px',
-                                         'minWidth': '900px', 'width': '900px', 'maxWidth': '900px',
-                                         'marginLeft': 'auto', 'marginRight': 'auto',
-                                         'marginTop': 0, 'marginBottom': "30"}
-                                     )
+df_weather = pd.read_csv('../data/weather.csv')
+df_weather.head()
 
 
 #creating the bar graph
-fig = px.bar(df_countries, 
-             x='year', 
-             y='lifeExp',  
-             color='country',
+fig = px.bar(df_weather, 
+             x='week-year', 
+             y='weekly_avg',  
+             color='city',
              barmode='group',
-             height=300, title = "Germany vs Denmark & Belgium",)
-
-fig = fig.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white", 
-    #margin=dict(l=20, r=20, t=0, b=20)
-)
-    
+             height=500, title = 'Average temperature',)
+fig.update_xaxes(tickangle=45)   
 
 graph = dcc.Graph(figure=fig)
 
-
 #creating the line graph
-fig2 = px.line(df_germany, x='year', y='lifeExp', height=300, title="Life Expectancy in Germany", markers=True)
-fig2 = fig2.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white"
-    )
+fig2 = px.line(df_weather, x='week-year', y='weekly_avg', color='city', title = 'Temperature over years',)
+fig2.update_xaxes(tickangle=45)
+fig2.write_html('weekly_temp.html')
 graph2 = dcc.Graph(figure=fig2)
 
 
 #creating the map
-fig3 = px.choropleth(df_countries, locations='iso_alpha', 
-                    projection='natural earth', animation_frame="year",
-                    scope='europe',  
-                    color='lifeExp', locationmode='ISO-3', 
-                    color_continuous_scale=px.colors.sequential.ice)
-
-fig3 = fig3.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white", geo_bgcolor="#222222"
-    )
+fig3 = px.choropleth(df_weather, locations='alpha-3',
+                    projection='natural earth',
+                    animation_frame='week-year',
+                    scope='world',
+                    color='weekly_avg', locationmode='ISO-3',
+                    title = 'Climat in Germany, Italy and USA')
+fig3.write_html('map.html')
 graph3 = dcc.Graph(figure=fig3)
 
+# Add a dropdown component to the bar graph
 
-# using the app with radio item
-app =dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
-server = app.server
+dropdown = dcc.Dropdown(['Berlin', 'Rome', 'New York'], "Berlin", clearable=False)
 
-radio= dcc.RadioItems(id="countries",options=['Germany', 'Belgium', 'Denmark'], value="Germany", 
-                      inline=True, style ={'paddingLeft': '30px'})
+app =dash.Dash()
 
-
-app.layout = html.Div([html.H1('Gap Minder Analysis of Germany', style={'textAlign': 'center', 'color': '#636EFA'}), 
-                       html.Div(html.P("Using the gapminder data we take a look at Germany's profile"), 
-                                style={'marginLeft': 50, 'marginRight': 25}),
-                       html.Div([html.Div('Germany', 
-                                          style={'backgroundColor': '#636EFA', 'color': 'white', 
-                                                 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
-                                 table, radio, graph,  graph2, graph3])
+app.layout = html.Div([html.H1(children = 'Climate in Berlin, Rome and New York', style = {'textAlign': 'center',
+                                                                        'color': 'purple'}),
+                       html.H2(children = 'Weekly temperature', style = {'paddingLeft':'30px'}),dropdown,graph, graph2, graph3
                       ])
 
 
 @callback(
-    Output(graph, "figure"), 
-    Input("countries", "value"))
+    Output(graph,'figure'), 
+    Input(dropdown, 'value'))
 
-#let's also define discrete colors for each bar, so we can distinguish them easily, everytime we change our selection
-
-def update_bar_chart(country): 
-    mask = df_countries["country"]==(country)
-    fig =px.bar(df_countries[mask], 
-             x='year', 
-             y='lifeExp',  
-             color='country',
-             color_discrete_map = {'Germany': '#7FD4C1', 'Denmark': '#8690FF', 'Belgium': '#F7C0BB'},
+def update_bar_chart(city): 
+    mask = df_weather['city'] == city
+    fig =px.bar(df_weather[mask], 
+             x='week-year', 
+             y='weekly_avg',  
+             color='city',
              barmode='group',
-             height=300, title = "Germany vs Denmark & Belgium",)
-    fig = fig.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white"
-    )
-
+             height=600, 
+             width=1000,
+             title = 'Average temperature')
+    fig.update_xaxes(tickangle=45)
     return fig 
 
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run_server()
