1b0b77455ebfc7e862e8d5443c517e077393610e
asia-ko <zykova.doc@gmail.com>
2023-11-09 13:21:47 +0100

    diabetes

---
+++ diabetes.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+import dash
+from dash import Dash, dcc, html, callback
+from dash.dependencies import Input, Output, State
+import plotly.express as px
+from dash import dash_table
+import dash_bootstrap_components as dbc
+
+
+diabetes_train = pd.read_csv('./data/diabbetes_train.csv')
+
+fig = px.scatter_3d(diabetes_train, x='HbA1c_level', y='diabetes', z='blood_glucose_level',
+              color='HbA1c_level',
+                   size_max=40,
+                   opacity=0.7)
+
+fig.update_layout(margin=dict(l=1, r=1, b=1, t=1))
+
+graph = dcc.Graph(figure=fig)
+
+app =dash.Dash()
+
+server = app.server
+
+if __name__ == '__main__':
+    app.run_server()
